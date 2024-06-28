8bc04f20d90bad854738bd60f6b9124a0d1f2fd5
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-06-28 12:59:53 +0530

    Completed the whole gsap project

---
+++ script.js
@@ -0,0 +1,26 @@
+var all = gsap.timeline()
+
+
+all.from('#nav h3',{
+  y: -50,
+  opacity:0,
+  delay:0.3,
+  duration:0.4,
+  stagger:0.2,
+})
+
+all.from('#main h1',{
+  x: -500,
+  opacity:0,
+  delay:0.5,
+  duration:0.8,
+  stagger:0.4,
+})
+
+all.from('img',{
+  x:100,
+  rotate:45,
+  opacity:0,
+  duration:0.5,
+  stagger:0.4,
+})
